3753d0f3a6a9c1bb97b7565de5bf5ce09a39e3c9
Walid Jamarin <walidjamarin21@gmail.com>
2021-04-04 16:55:15 +0800

    added redux persist to cache our cart items when refreshing our page and persist everytime

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
-
+import { persistStore } from 'redux-persist';
 const middlewares = [logger];
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-
-export default store;
+const persistor = persistStore(store);
+const exportedFunction = {store, persistor};
+export default exportedFunction;
