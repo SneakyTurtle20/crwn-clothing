f84374fa33d09e2c6f84fae5f6fbda20720c4906
Walid Jamarin <walidjamarin21@gmail.com>
2021-04-14 21:34:14 +0800

    created user and cart sagas for sign in, sign up and sign in with google, implement our redux saga for user, cart and shop reducers

--- src/App.js
+++ src/App.js
@@ -10,28 +10,15 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 import Header from './components/header/header.component';
 import CheckoutPage from './pages/checkout/checkout.component';
 
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-import { setCurrentUser } from './redux/user/user.action';
 import { selectCurrentUser } from './redux/user/user.selectors'
+import { checkUserSession } from './redux/user/user.actions';
 class App extends React.Component {
 
   unsubcribeFromAuth = null;
 
   componentDidMount() {
-    const {setCurrentUser} = this.props;
-    this.unsubcribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      if(userAuth) {
-        const userRef = await createUserProfileDocument(userAuth);
-        
-        userRef.onSnapshot(snapShot => {
-          setCurrentUser({
-              id: snapShot.id,
-              ...snapShot.data()
-          });
-        });
-      }
-      setCurrentUser(userAuth);
-    });
+    const { checkUserSession } = this.props;
+    checkUserSession();
   }
 
   componentWillUnmount() {
@@ -66,7 +53,8 @@ const mapStateToProps = createStructuredSelector({
 });
 
 const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
+  checkUserSession: () => dispatch(checkUserSession())
 });
+
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
+// import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 import { SignInContainer, ButtonsContainer, SignInTitle } from './sign-in.styles';
+import { googleSignInStart, emailSignInStart } from '../../redux/user/user.actions';
 class SignIn extends React.Component {
   constructor(props) {
     super(props);
@@ -14,15 +16,17 @@ class SignIn extends React.Component {
 
   handleSubmit = async event => {
     event.preventDefault();
-
+    const  { emailSignInStart } = this.props;
     const { email, password } = this.state;
 
-    try {
-      await auth.signInWithEmailAndPassword(email, password);
-      this.setState({ email: '', password: '' });
-    } catch (error) {
-      console.log(error);
-    }
+    // try {
+    //   await auth.signInWithEmailAndPassword(email, password);
+    //   this.setState({ email: '', password: '' });
+    // } catch (error) {
+    //   console.log(error);
+    // }
+
+    emailSignInStart(email, password);
   }
   
   handleChange = event => {
@@ -32,6 +36,7 @@ class SignIn extends React.Component {
   }
 
   render() {
+    const { googleSignInStart } = this.props;
     return (
       <SignInContainer>
       <SignInTitle>I have an account</SignInTitle>
@@ -54,7 +59,7 @@ class SignIn extends React.Component {
           required />
       <ButtonsContainer>
         <CustomButton type="submit">Sign In</CustomButton>
-        <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn>{' '} 
+        <CustomButton type="button" onClick={googleSignInStart} isGoogleSignIn>{' '} 
         Sign in with Google { ' ' }
         </CustomButton>
       </ButtonsContainer>
@@ -64,4 +69,10 @@ class SignIn extends React.Component {
   }
 }
 
-export default SignIn;
+const mapDispatchToProps = dispatch => ({
+  googleSignInStart: () => dispatch(googleSignInStart()),
+  emailSignInStart: (email, password) => 
+    dispatch(emailSignInStart({ email, password }))
+});
+
+export default connect(null, mapDispatchToProps)(SignIn);
